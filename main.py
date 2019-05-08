903e9d8a6438f6dbdd5c78b88b55c05872219ae6
myeongjun <leaderj1001@gmail.com>
2019-05-08 15:26:12 +0900

    [Create] init commit

---
+++ main.py
@@ -0,0 +1,143 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from preprocess import load_data
+from model import MobileNetV3
+
+import argparse
+from tqdm import tqdm
+import time
+import os
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def get_args():
+    parser = argparse.ArgumentParser("parameters")
+
+    parser.add_argument("--dataset-mode", type=str, default="CIFAR100", help="(example: CIFAR10, CIFAR100, MNIST), (default: CIFAR100)")
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs, (default: 100)")
+    parser.add_argument("--batch-size", type=int, default=100, help="number of batch size, (default, 8)")
+    parser.add_argument("--learning-rate", type=float, default=1e-1, help="learning_rate, (default: 1e-1)")
+    parser.add_argument("--dropout", type=float, default=0.3, help="dropout rate, (default: 0.3)")
+    parser.add_argument("--load-pretrained", type=bool, default=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def train(model, train_loader, optimizer, criterion, epoch, args):
+    model.train()
+    step = 0
+    train_loss = 0
+    train_acc = 0
+    for data, target in tqdm(train_loader, desc="epoch " + str(epoch), mininterval=1):
+        adjust_learning_rate(optimizer, epoch, args)
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.data
+        y_pred = output.data.max(1)[1]
+
+        acc = float(y_pred.eq(target.data).sum()) / len(data) * 100.
+        train_acc += acc
+        step += 1
+        if step % 100 == 0:
+            print("[Epoch {0:4d}] Loss: {1:2.3f} Acc: {2:.3f}%".format(epoch, loss.data, acc), end='')
+            for param_group in optimizer.param_groups:
+                print(",  Current learning rate is: {}".format(param_group['lr']))
+
+    length = len(train_loader.dataset) // args.batch_size
+    return train_loss / length, train_acc / length
+
+
+def get_test(model, test_loader):
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for data, target in tqdm(test_loader, desc="evaluation", mininterval=1):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            prediction = output.data.max(1)[1]
+            correct += prediction.eq(target.data).sum()
+
+    acc = 100. * float(correct) / len(test_loader.dataset)
+    return acc
+
+
+def main():
+    args = get_args()
+    train_loader, test_loader = load_data(args)
+
+    if args.dataset_mode is "CIFAR10":
+        num_classes = 10
+    elif args.dataset_mode is "CIFAR100":
+        num_classes = 100
+    elif args.dataset_mode is "MNIST":
+        num_classes = 10
+
+    if args.load_pretrained:
+        model = MobileNetV3().to(device)
+        filename = "best_model_"
+        checkpoint = torch.load('./checkpoint/' + filename + 'ckpt.t7')
+        model.load_state_dict(checkpoint['model'])
+        epoch = checkpoint['epoch']
+        acc = checkpoint['acc']
+        max_test_acc = acc
+        print("Load Model Accuracy: ", acc, "Load Model end epoch: ", epoch)
+    else:
+        model = MobileNetV3().to(device)
+        epoch = 1
+        max_test_acc = 0
+    # if device is "cuda":
+    #     model = nn.DataParallel(model)
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=5e-4, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    if not os.path.isdir("reporting"):
+        os.mkdir("reporting")
+
+    start_time = time.time()
+    for epoch in range(epoch, args.epochs):
+        train(model, train_loader, optimizer, criterion, epoch, args)
+        test_acc = get_test(model, test_loader)
+        if max_test_acc < test_acc:
+            print('Saving..')
+            state = {
+                'model': model.state_dict(),
+                'acc': test_acc,
+                'epoch': epoch,
+            }
+            if not os.path.isdir('checkpoint'):
+                os.mkdir('checkpoint')
+            filename = "best_model_"
+            torch.save(state, './checkpoint/' + filename + 'ckpt.t7')
+            max_test_acc = test_acc
+
+        time_interval = time.time() - start_time
+        time_split = time.gmtime(time_interval)
+        print("Training time: ", time_interval, "Hour: ", time_split.tm_hour, "Minute: ", time_split.tm_min, "Second: ", time_split.tm_sec, end='')
+        print(" Test acc:", max_test_acc, "time: ", time.time() - start_time)
+        with open("./reporting/" + "best_model.txt", "w") as f:
+            f.write("Epoch: " + str(epoch) + " " + "Best acc: " + str(max_test_acc) + "\n")
+            f.write("Training time: " + str(time_interval) + "Hour: " + str(time_split.tm_hour) + "Minute: " + str(
+                time_split.tm_min) + "Second: " + str(time_split.tm_sec))
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+
+def _weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, math.sqrt(2. / n))
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+    elif isinstance(m, nn.Linear):
+        n = m.weight.size(1)
+        m.weight.data.normal_(0, 0.01)
+        m.bias.data.zero_()
+
+
+class bneck(nn.Module):
+    def __init__(self, in_channels, out_channels, kernal_size, stride, HS, RE, SE, dense_layer, padding=1):
+        super(bneck, self).__init__()
+        self.out_channels = out_channels
+        self.HS = HS
+        self.RE = RE
+        self.SE = SE
+
+        self.use_connect = stride == 1 and in_channels == out_channels
+
+        if self.RE == True:
+            self.activation_RE = nn.ReLU(inplace=True)
+        elif self.HS == True:
+            self.activation_HS = nn.ReLU6(inplace=True)
+
+        if self.SE == True:
+            self.squeeze = nn.Sequential(
+                nn.Linear(in_channels, dense_layer),
+                nn.ReLU(inplace=True),
+                nn.Linear(dense_layer, in_channels),
+            )
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernal_size, stride=stride, padding=padding, groups=in_channels),
+            nn.BatchNorm2d(in_channels),
+        )
+
+        self.conv1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        # MobileNetV2
+        output = self.conv(x)
+        if self.RE == True:
+            output = self.activation_RE(output)
+        elif self.HS == True:
+            output = self.activation_HS(output + 3) / 6 * output
+        batch, channels, height, width = output.size()
+        original_output = output
+        if self.SE == True:
+            # Squeeze and Excite
+            global_pooled_output = F.avg_pool2d(output, kernel_size=[height, width])
+            global_pooled_output = torch.reshape(global_pooled_output, shape=(-1, channels))
+            squeeze = self.squeeze(global_pooled_output)
+            squeeze = torch.reshape(squeeze, shape=(-1, channels, 1, 1))
+            squeeze = torch.sigmoid(squeeze) * squeeze
+            output = original_output * squeeze
+
+        output = self.conv1x1(output)
+        if self.RE == True:
+            output = self.activation_RE(output)
+        elif self.HS == True:
+            output = self.activation_HS(output + 3) / 6 * output
+        if self.use_connect:
+            return x + output
+        else:
+            return output
+
+
+class MobileNetV3(nn.Module):
+    def __init__(self, model_mode="LARGE"):
+        super(MobileNetV3, self).__init__()
+        self.activation_HS = nn.ReLU6(inplace=True)
+
+        if model_mode == "LARGE":
+            self.init_conv = nn.Sequential(
+                nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1),
+                nn.BatchNorm2d(16),
+            )
+
+            self.block = nn.Sequential(
+                bneck(in_channels=16, out_channels=16, kernal_size=3, stride=1, HS=False, RE=True, SE=False, dense_layer=16),
+                bneck(in_channels=16, out_channels=24, kernal_size=3, stride=2, HS=False, RE=True, SE=False, dense_layer=64),
+                bneck(in_channels=24, out_channels=24, kernal_size=3, stride=1, HS=False, RE=True, SE=False, dense_layer=72),
+                bneck(in_channels=24, out_channels=40, kernal_size=5, stride=2, HS=False, RE=True, SE=True, dense_layer=72, padding=2),
+                bneck(in_channels=40, out_channels=40, kernal_size=5, stride=1, HS=False, RE=True, SE=True, dense_layer=120, padding=2),
+                bneck(in_channels=40, out_channels=40, kernal_size=5, stride=1, HS=False, RE=True, SE=True, dense_layer=120, padding=2),
+                bneck(in_channels=40, out_channels=80, kernal_size=3, stride=2, HS=True, RE=False, SE=False, dense_layer=240),
+                bneck(in_channels=80, out_channels=80, kernal_size=3, stride=1, HS=True, RE=False, SE=False, dense_layer=200),
+                bneck(in_channels=80, out_channels=80, kernal_size=3, stride=1, HS=True, RE=False, SE=False, dense_layer=184),
+                bneck(in_channels=80, out_channels=80, kernal_size=3, stride=1, HS=True, RE=False, SE=False, dense_layer=184),
+                bneck(in_channels=80, out_channels=112, kernal_size=3, stride=1, HS=True, RE=False, SE=True, dense_layer=480),
+                bneck(in_channels=112, out_channels=112, kernal_size=3, stride=1, HS=True, RE=False, SE=True, dense_layer=672),
+                bneck(in_channels=112, out_channels=160, kernal_size=5, stride=1, HS=True, RE=False, SE=True, dense_layer=672, padding=2),
+                bneck(in_channels=160, out_channels=160, kernal_size=5, stride=2, HS=True, RE=False, SE=True, dense_layer=672, padding=2),
+                bneck(in_channels=160, out_channels=160, kernal_size=5, stride=1, HS=True, RE=False, SE=True, dense_layer=960, padding=2),
+            )
+
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(160, 960, kernel_size=1, stride=1),
+                nn.BatchNorm2d(960),
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(960, 1280, kernel_size=1, stride=1),
+            )
+            self.conv3 = nn.Sequential(
+                nn.Conv2d(1280, 100, kernel_size=1, stride=1),
+            )
+
+        elif model_mode == "SMALL":
+            pass
+
+        self.apply(_weights_init)
+
+    def forward(self, x):
+        output = self.init_conv(x)
+        output = self.activation_HS(output + 3) / 6 * output
+        output = self.block(output)
+        output = self.conv1(output)
+        output = self.activation_HS(output + 3) / 6 * output
+        batch, channels, height, width = output.size()
+        output = F.avg_pool2d(output, kernel_size=[height, width])
+        output = self.conv2(output)
+        output = self.activation_HS(output + 3) / 6 * output
+        output = self.conv3(output)
+        output = torch.reshape(output, shape=(-1, 100))
+        return output
+
+
+# temp = torch.zeros((1, 3, 224, 224))
+# model = MobileNetV3()
+# print(model(temp).shape)
