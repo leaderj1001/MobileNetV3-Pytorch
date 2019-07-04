937b983c7e57e1b495e52aa3d5ee230da98ced3d
Myeongjun Kim <leaderj1001@gmail.com>
2019-07-04 09:36:15 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -48,14 +48,14 @@ def load_data(args):
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
         train_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR100('data', train=True, download=True, transform=transform_train),
+            datasets.CIFAR10('data', train=True, download=True, transform=transform_train),
             batch_size=args.batch_size,
             shuffle=True,
             num_workers=args.workers
         )
 
         test_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR100('data', train=False, transform=transform_test),
+            datasets.CIFAR10('data', train=False, transform=transform_test),
             batch_size=args.batch_size,
             shuffle=False,
             num_workers=args.workers
